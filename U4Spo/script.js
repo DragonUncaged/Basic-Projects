dc73e588a498b78cae24086d945014a763aa514e
Ankit Rao <72241207+DragonUncaged@users.noreply.github.com>
2024-12-15 15:15:12 +0530

    Add files via upload

---
+++ U4Spo/script.js
@@ -0,0 +1,21 @@
+async function getPublicIp(){
+    try {
+        const ipUrl = 'https://api.ipify.org?format=json';
+
+        let response = await fetch(ipUrl);
+        let data = await response.json();
+
+        ip.innerText = data.ip;
+        localStorage.setItem("myIp", data.ip);
+    }
+    catch(error) {
+        console.log(error);
+    }
+}
+getPublicIp();
+
+document.querySelector('.start-btn').addEventListener('click', () => {
+    let link = document.createElement('a');
+    link.href = 'details.html';
+    link.click();
+})
