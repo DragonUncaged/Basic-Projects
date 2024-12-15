997973fa73f595865c34ff4c123096d1a5893610
Ankit Rao <72241207+DragonUncaged@users.noreply.github.com>
2024-12-15 19:54:09 +0530

    Added

---
+++ U4Sca/login/login.js
@@ -0,0 +1,22 @@
+function loginProcess() {
+  // getting data from local storage
+  const UsrSavedData = JSON.parse(localStorage.getItem("Usr"));
+  var email = document.getElementById("email").value;
+  var password = document.getElementById("pass").value;
+
+  if (!UsrSavedData) {
+    alert("No user data found or No one has singed up yet...");
+    return;
+  }
+  const curntUser = UsrSavedData.find(user => user.email === email && user.password === password)
+
+  // if user is found then add it in localStorage as a curntUser
+  // and render to the shopping page
+  if (curntUser) {
+    localStorage.setItem("curntUser", JSON.stringify(curntUser));
+    window.location.href = "../shop/shop.html";
+  }
+  else {
+    alert("Enter a correct email or password");
+  }
+}
